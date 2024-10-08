ff7bd2a01c034cb88c478ef7d53e668a5e0173a6
Victoria Cano <victoriacano@outlook.es>
2024-10-07 21:33:19 -0500

    variables

---
+++ parcial2punto1/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int numAleatorio = 0;
+        int numJugadores = 0;
+        bool playAgain = true;
